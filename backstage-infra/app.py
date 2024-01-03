78652de13eb2c2eb11821f623332ae700cde5aad
rlbth <rlb.hps@gmail.com>
2024-01-02 20:59:56 -0800

    changed image pull policy

--- backstage-infra/app.py
+++ backstage-infra/app.py
@@ -3,12 +3,10 @@ import os
 import aws_cdk as cdk
 from constructs import Construct
 
-from backstage_infra.rds_stack import BackstageRDSStack
 from backstage_infra.eks_infra import BackstageEksClusterStack
 
 aws_account=cdk.Environment(account='938361207488', region='eu-west-1')
 app = cdk.App()
 BackstageEksClusterStack(scope=app, id="EKSCluster", description ='Sets up an EKS cluster and a fargate profile', env=aws_account)
-BackstageRDSStack(scope=app, id="RDSDB", description = 'Sets up an Posgres RDS instance', env=aws_account )
 app.synth()
 
